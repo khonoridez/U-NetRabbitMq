32d724b04d20444a43af4eab8e00dd1a187c7b4e
khonoridez <oznyakis@gmail.com>
2019-12-24 12:42:16 -0500

    Create Transfer Service and Repository

---
+++ MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Transfer.Data.Repository
+{
+    public class TransferRepository : ITransferRepository
+    {
+        private readonly TransferDbContext _ctx;
+
+        public TransferRepository(TransferDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs() => _ctx.TransferLogs;
+    }
+}
