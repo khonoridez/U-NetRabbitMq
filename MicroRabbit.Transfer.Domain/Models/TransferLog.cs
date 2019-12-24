383909471222588402781f86025f996a50c16dd5
khonoridez <oznyakis@gmail.com>
2019-12-24 10:31:51 -0500

    Setup Transfer Microservice - Api, Domain Models, DbContext and Ef migration

---
+++ MicroRabbit.Transfer.Domain/Models/TransferLog.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroRabbit.Transfer.Domain.Models
+{
+    public class TransferLog
+    {
+        public int Id { get; set; }
+        public string AccountType { get; set; }
+        public decimal AccountBalance { get; set; }
+        public decimal TransferAmount { get; set; }
+    }
+}
