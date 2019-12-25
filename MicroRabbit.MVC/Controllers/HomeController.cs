c3e2322d71c059e7581cef1051c73153da32f5b7
khonoridez <oznyakis@gmail.com>
2019-12-24 19:20:41 -0500

    Implement Transfer Post method in Controller, Implement View

--- MicroRabbit.MVC/Controllers/HomeController.cs
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -1,20 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Models.DTO;
+using MicroRabbit.MVC.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MicroRabbit.MVC.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace MicroRabbit.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ITransferService _transferService;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ITransferService transferService, ILogger<HomeController> logger)
         {
+            _transferService = transferService;
             _logger = logger;
         }
 
@@ -32,6 +33,21 @@ namespace MicroRabbit.MVC.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDto transferDto = new TransferDto
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDto);
+
+            return View("Index");
         }
     }
 }
